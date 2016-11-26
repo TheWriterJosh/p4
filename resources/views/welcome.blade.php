1eb4409706bbcf4361e0fe69ece363def1263287
Josh Anderson <Josh@Joshuas-MBP-2.fios-router.home>
2016-11-26 15:17:49 -0500

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -75,7 +75,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Bucket List Manager
                 </div>
 
                 <div class="links">
